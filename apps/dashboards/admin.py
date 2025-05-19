0f20c43a452654e628851630ef343b1a35d94088
Imee <vitoimee@gmail.com>
2025-05-19 17:31:37 +0800

    Added initial data

--- apps/dashboards/admin.py
+++ apps/dashboards/admin.py
@@ -1,2 +1,19 @@
+from django.contrib import admin
+from .models import GradeLevel, Club, Student, ClubMember
 
-# Register your models here.
+admin.site.register(GradeLevel)
+admin.site.register(Club)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "grade_level")
+    search_fields = ("lastname", "firstname")
+
+@admin.register(ClubMember)
+class ClubMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_student_grade_level", "club", "date_joined")
+    search_fields = ("student__lastname", "student__firstname")
+
+    @admin.display(description="Grade Level")
+    def get_student_grade_level(self, obj):
+        return obj.student.grade_level
--- apps/dashboards/models.py
+++ apps/dashboards/models.py
@@ -1,2 +1,36 @@
+from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class GradeLevel(BaseModel):
+    level_name = models.CharField(max_length=50)  # e.g., "Grade 7", "Grade 10"
+
+    def __str__(self):
+        return self.level_name
+
+class Club(BaseModel):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+class Student(BaseModel):
+    student_id = models.CharField(max_length=15)
+    lastname = models.CharField(max_length=25)
+    firstname = models.CharField(max_length=25)
+    middlename = models.CharField(max_length=25, blank=True, null=True)
+    grade_level = models.ForeignKey(GradeLevel, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.lastname}, {self.firstname}"
+
+class ClubMember(BaseModel):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+    date_joined = models.DateField()
